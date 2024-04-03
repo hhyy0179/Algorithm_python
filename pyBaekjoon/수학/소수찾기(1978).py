0973ffd2de79d7a15033ff07680db7d050017c7e
hhyy0179 <rharhazbvl@gmail.com>
2024-04-03 16:05:00 +0900

    2024.04.03

--- pyBaekjoon/수학/소수찾기(1978).py
+++ pyBaekjoon/수학/소수찾기(1978).py
@@ -4,12 +4,15 @@ cnt = 0
 
 def find_prime(n):
     flag = True
+    if n == 1:
+        return False
     for div in range(2,n):
         if (n % div == 0):
             flag = False
     return flag
 
 for n in numbers:
+    if find_prime(n):
+        cnt+=1
     
-
-
+print(cnt)
